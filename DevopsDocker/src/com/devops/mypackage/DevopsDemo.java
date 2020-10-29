1855f649a7cbb3fb3131efba4150541b4f4f47f3
Rishabh Kumar <kumar.90rishabh@gmail.com>
2020-10-29 00:24:19 +0000

    First Commit

---
+++ DevopsDocker/src/com/devops/mypackage/DevopsDemo.java
@@ -0,0 +1,12 @@
+package com.devops.mypackage;
+
+public class DevopsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is the output from the DevopsDemo class running using Docker");
+
+	}
+
+}
